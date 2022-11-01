a195dfcdef23f1727dacba2e4816566035d0a396
prowebdev110 <tieosong119@gmail.com>
2022-11-01 09:06:53 +0900

    Update productview.js

--- client/inventory/productview.js
+++ client/inventory/productview.js
@@ -3565,7 +3565,6 @@ Template.productview.events({
                             templateObject.isTrackChecked.set(false);
                         }
                     });
-                    // swal('PLEASE NOTE', 'If Inventory Tracking is turned on, it cannot be turned off for this product in the future.', 'info');
                 }
             }
 
